b2812be1997cf21789254290616ba651a414fa7d
Hari Nath <hari@testpress.in>
2026-06-30 17:37:50 +0530

    ui: Show app name with logo

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,12 +1,22 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { appName, gitConfig } from './shared';
+import { appName } from './shared';
+import Image from "next/image"
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      // JSX supported
-      title: appName,
+      title: (
+        <div title={appName} className="flex items-center gap-2.5 text-stone-800 dark:text-stone-100">
+          <Image
+            src="/favicon-32x32.png"
+            alt="TPSentinel Logo"
+            width={24}
+            height={24}
+            className="rounded-md"
+          />
+          <span className="font-medium">{appName}</span>
+        </div>
+      ),
     },
-    githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
 }
